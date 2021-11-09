fe848bcbd25e715f9f1220cf8907ae7374e70b90
patrick <patrick@o-neill.ca>
2021-11-09 08:35:33 -0500

    added pseudocode

---
+++ mastermind.rb
@@ -0,0 +1,7 @@
+## codemaker class chooses color sequence
+
+## codebreaker class guesses sequence
+
+## game class runs turns together, gives feedback on guesses, announces winner,
+
+## game runner class, instnatiates the other classes and starts the game
